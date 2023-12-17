63c2a7420bc69c171111d451173a92b62fb32750
bouharri <bouharri@nmu.edu>
2023-12-17 15:52:25 -0500

    feat: added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import react from "@astrojs/react"
+import robotsTxt from "astro-robots-txt"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), robotsTxt(), sitemap(), tailwind()],
-  site: "https://harrisonbouche.com",
-});
+	integrations: [mdx(), react(), robotsTxt(), sitemap(), tailwind()],
+	site: "https://harrisonbouche.com",
+})
