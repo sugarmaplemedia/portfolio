aa4bf5369efacc55a842c335211aa3d12c117da5
bouharri <bouharri@nmu.edu>
2023-12-17 15:53:04 -0500

    chore: added missing gap

--- src/components/structure/Columns.tsx
+++ src/components/structure/Columns.tsx
@@ -3,7 +3,7 @@ import React, { type PropsWithChildren } from "react"
 type Props = PropsWithChildren<{ count?: number }>
 export default ({ count = 2, children }: Props) => (
 	<div
-		className="grid w-full"
+		className="grid w-full gap-6"
 		style={{ gridTemplateColumns: `repeat(${count}, 1fr)` }}
 	>
 		{children}
